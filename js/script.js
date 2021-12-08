ed62382064a5d03fd100e141d2bedca7604ca98b
Glitch (rock-pap-scissors) <none>
2021-12-08 23:26:08 +0000

    👞👰 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 $(".play").click(function(){
  let game= $(".input").val();
   $(".userChoice").text(game);
-});
+  let randomNumber=Math.ceil(Math.random() * 3);
+  console.log(randomNumber);
 
-let randomNumber = 0;
-$(".play").click(function(){
-  let randomNumber=Math.random();
-  
+  let computerChoice=;
+  if(randomNumber===1){
+    $(".computerChoice").text("rock");
+     }
+  else if(randomNumber===2){
+    $(".computerChoice").text("paper");
+     }
+  else if(randomNumber===3){
+    $(".computerChoice").text("scissors");
+     }
+
 });
